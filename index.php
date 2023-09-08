5590c84c759f3d641a2bf0851192e6791221838c
Ailerssi <danila.kuratnik1@gmail.com>
2023-09-09 00:50:52 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,236 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Резюме php developer">
+    <meta name="author" content="Куратник">
+    <title>Резюме Куратник</title>
+    <!-- Favicon icon -->
+    <link rel="icon" href="assets/favicon/favicon-32x32.png"> <!-- 32×32 -->
+    <link rel="apple-touch-icon" href="assets/favicon/apple-touch-icon.png">  <!-- 180×180 -->
+    <!-- font icons -->
+    <link rel="stylesheet" href="assets/vendors/themify-icons/css/themify-icons.css">
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <!-- Bootstrap + Meyawo main styles -->
+    <link rel="stylesheet" href="assets/css/meyawo.css">
+</head>
+
+<body data-spy="scroll" data-target=".navbar" data-offset="40" id="home">
+
+    <!-- Page Navbar -->
+    <nav class="custom-navbar" data-spy="affix" data-offset-top="20">
+        <div class="container">
+            <a class="logo" href="#">Kuratnik</a>
+            <ul class="nav">
+                <li class="item">
+                    <a class="link" href="#home">Главная</a>
+                </li>
+                <li class="item">
+                    <a class="link" href="#about">Обо мне</a>
+                </li>
+                <li class="item">
+                    <a class="link" href="#portfolio">Портфолио</a>
+                </li>
+                <li class="item">
+                    <a class="link" href="#contact">Связаться со мной</a>
+                </li>
+            </ul>
+            <a href="javascript:void(0)" id="nav-toggle" class="hamburger hamburger--elastic">
+                <div class="hamburger-box">
+                    <div class="hamburger-inner"></div>
+                </div>
+            </a>
+        </div>
+    </nav><!-- End of Page Navbar -->
+
+    <!-- page header -->
+    <header id="home" class="header">
+        <div class="overlay"></div>
+        <div class="header-content container">
+            <h1 class="header-title">
+                <span class="up">ПРИВЕТ!</span>
+                <span class="down">Я Куратник Данила</span>
+            </h1>
+            <p class="header-subtitle">PHP DEVELOPER</p>
+
+            <a href="#portfolio"><button class="btn btn-primary">Мои работы</button></a>
+        </div>
+    </header><!-- end of page header -->
+
+    <!-- about section -->
+    <section class="section pt-0" id="about">
+        <!-- container -->
+        <div class="container text-center">
+            <!-- about wrapper -->
+            <div class="about">
+                <div class="about-img-holder">
+                    <img src="assets/imgs/iam.jpg" class="about-img"
+                        alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, meyawo Landing page">
+                </div>
+                <div class="about-caption">
+                    <p class="section-subtitle">Краткое содержание</p>
+                    <h2 class="section-title mb-3">Обо мне</h2>
+                    <p>
+                        Привет, я молодой и амбициозный PHP-разработчик с опытом  <script>document.write(new Date().getFullYear()-2021)</script> года. Я страстно увлечен созданием веб-приложений с использованием PHP, особенно в фреймворках, таких как Laravel. Моя магия проявляется в работе с базами данных, где MySQL - мой верный спутник.<br>
+                        Когда речь заходит о внешнем облике, я превращаю макеты в живые и красочные веб-интерфейсы, играя с HTML, CSS и JavaScript.<br>
+                        Я верю в масштабируемость и производительность приложений и всегда стараюсь сделать их быстрее и эффективнее. Готов воплощать новые идеи и вызовы, чтобы делать веб-мир более интересным и функциональным.
+                    </p>
+                    <a href="/assets/files/Резюме Данила Куратник.pdf" class="btn-rounded btn btn-outline-primary mt-4" download="">Скачать резюме</a>
+                </div>
+            </div><!-- end of about wrapper -->
+        </div><!-- end of container -->
+    </section> <!-- end of about section -->
+
+    <!-- service section -->
+    <section class="section" id="service">
+        <div class="container text-center">
+            <h6 class="section-title mb-6">Hard skills</h6>
+            <!-- row -->
+            <div class="row">
+                <div class="col-md-6 col-lg-3">
+                    <div class="service-card">
+                        <div class="body">
+                            <img src="assets/imgs/pencil-case.svg"
+                                alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, meyawo Landing page"
+                                class="icon">
+                            <h6 class="title">Frontend</h6>
+                            <p class="subtitle">HTML, CSS,
+                                JavaScript</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="service-card">
+                        <div class="body">
+                            <img src="assets/imgs/responsive.svg"
+                                alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, meyawo Landing page"
+                                class="icon">
+                            <h6 class="title">Back-end</h6>
+                            <p class="subtitle">PHP,
+                                Laravel, Wordpress
+                                </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="service-card">
+                        <div class="body">
+                            <img src="assets/imgs/toolbox.svg"
+                                alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, meyawo Landing page"
+                                class="icon">
+                            <h6 class="title">Tools</h6>
+                            <p class="subtitle">Phpmyadmin, phpstorm, git, gitlab</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3">
+                    <div class="service-card">
+                        <div class="body">
+                            <img src="assets/imgs/analytics.svg"
+                                alt="Download free bootstrap 4 landing page, free boootstrap 4 templates, Download free bootstrap 4.1 landing page, free boootstrap 4.1.1 templates, meyawo Landing page"
+                                class="icon">
+                            <h6 class="title">DB</h6>
+                            <p class="subtitle">SQL,
+                                MySql, MariaDB
+                                </p>
+                        </div>
+                    </div>
+                </div>
+            </div><!-- end of row -->
+        </div>
+    </section><!-- end of service section -->
+
+    <!-- portfolio section -->
+    <section class="section" id="portfolio">
+        <div class="container text-center">
+            <p class="section-subtitle">Что я делал ?</p>
+            <h6 class="section-title mb-6">Портфолио</h6>
+            <!-- row -->
+            <div class="row">
+                <div class="col-md-6">
+                    <a href="https://mixfor.by/" class="portfolio-card">
+                        <img src="assets/imgs/mixfor.png" class="portfolio-card-img">
+                        <span class="portfolio-card-overlay">
+                            <span class="portfolio-card-caption">
+                                <h4>Fullstack</h4>
+                                    <p class="font-weight-normal">Категория: Wordpress</p>
+                            </span>
+                        </span>
+                    </a>
+                </div>
+                <div class="col-md-6">
+                    <a href="" class="portfolio-card">
+                        <img class="portfolio-card-img" src="assets/imgs/project.png" class="img-responsive rounded">
+                        <span class="portfolio-card-overlay">
+                            <span class="portfolio-card-caption">
+                                <h4>PHP back-end</h4>
+                                    <p class="font-weight-normal">Категория: Laravel</p>
+                                    <a target="_blank" href="https://github.com/Ailerssi/project" class="link"><i class="ti-github"></i></a>
+                            </span>
+                        </span>
+                    </a>
+                </div>
+            </div><!-- end of row -->
+        </div><!-- end of container -->
+    </section> <!-- end of portfolio section -->
+
+    <!-- pricing section -->
+    <!-- contact section -->
+    <section class="section" id="contact">
+        <div class="container text-center">
+            <p class="section-subtitle">Как смо мной связаться?</p>
+            <h6 class="section-title mb-5">Напишите мне</h6>
+            <!-- contact form -->
+            <form action="" class="contact-form col-md-10 col-lg-8 m-auto">
+                <div class="form-row">
+                    <div class="form-group col-sm-6">
+                        <input type="text" size="50" class="form-control" placeholder="Ваше имя (название организации)" required>
+                    </div>
+                    <div class="form-group col-sm-6">
+                        <input type="email" class="form-control" placeholder="Почта" requried>
+                    </div>
+                    <div class="form-group col-sm-12">
+                        <textarea name="comment" id="comment" rows="6" class="form-control"
+                            placeholder="..."></textarea>
+                    </div>
+                    <div class="form-group col-sm-12 mt-3">
+                        <input type="submit" value="Send Message" class="btn btn-outline-primary rounded">
+                    </div>
+                </div>
+            </form><!-- end of contact form -->
+        </div><!-- end of container -->
+    </section><!-- end of contact section -->
+
+    <!-- footer -->
+    <div class="container">
+        <footer class="footer">
+            <p class="mb-0">Copyright
+                <script>document.write(new Date().getFullYear())</script> &copy; 
+                Kuratnik Danila
+            </p>
+            <div class="social-links text-right m-auto ml-sm-auto">
+                <a target="_blank" href="tel:+375447832022" class="link"><i class="ti-mobile"></i></a>
+                <a target="_blank" href="https://github.com/Ailerssi" class="link"><i class="ti-github"></i></a>
+                <a target="_blank" href="https://www.linkedin.com/in/danila-kuratnik-139536271/" class="link"><i class="ti-linkedin"></i></a>
+                <a target="_blank" href="https://t.me/Ailerssi" class="link"><svg style="margin-bottom: 4px;" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telegram" viewBox="0 0 16 16">
+                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z"/>
+                  </svg></a>
+            </div>
+        </footer>
+    </div> <!-- end of page footer -->
+
+    <!-- core  -->
+    <script src="assets/vendors/jquery/jquery-3.4.1.js"></script>
+    <script src="assets/vendors/bootstrap/bootstrap.bundle.js"></script>
+
+    <!-- bootstrap 3 affix -->
+    <script src="assets/vendors/bootstrap/bootstrap.affix.js"></script>
+
+    <!-- Meyawo js -->
+    <script src="assets/js/meyawo.js"></script>
+
+</body>
+
+</html>
